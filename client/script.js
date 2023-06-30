a0c42f5f3d267a52612927c894aa5edcd6253415
Gaurav kumar <105967889+iamgauarav@users.noreply.github.com>
2023-06-30 12:56:00 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5001', {
+  const response = await fetch('https://thing-ab7y.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
